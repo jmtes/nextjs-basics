0e106df5453a8575387a90b1678be53a80dea9aa
jmtes <juliennetesoro@gmail.com>
2020-11-24 15:36:41 -0800

    Add links to blog posts

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import Date from '../components/date';
+
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 
@@ -91,11 +93,13 @@ const Home = ({ allPostsData }) => (
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
             <br />
-            {id}
-            <br />
-            {date}
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
           </li>
         ))}
       </ul>
