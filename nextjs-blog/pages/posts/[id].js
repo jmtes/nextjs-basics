b539b48827550738d2341b1d3814c015a2dd42bb
jmtes <juliennetesoro@gmail.com>
2020-11-24 15:54:01 -0800

    Add notes on fallback

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -30,10 +30,27 @@ const Post = ({ postData: { title, date, contentHtml } }) => (
 export const getStaticPaths = async () => {
   const paths = getAllPostIds();
 
+  return { paths, fallback: false };
   // The array of possible values for `id` must be the value of the `paths`
   // key in the returned object, which is exactly what getAllPostIds()
   // returns.
-  return { paths, fallback: false };
+
+  // If fallback is false, then any paths not returned by getStaticPaths
+  // will result in a 404 page.
+  // You can create a custom 404 page by creating a `pages/404.js` file.
+  // See the error pages documentation to learn more.
+
+  // If fallback is true, then the behavior of getStaticProps changes:
+  // 1. Paths returned from getStaticPaths will be rendered to HTML at build
+  //    time.
+  // 2. The paths not generated at build time will NOT result in a 404 page
+  //    and instead, a "fallback" version of the page will be served on the
+  //    first request to such a path.
+  // 3. In the background, Next.js will statically generate the requested
+  //    path. Subsequent requests to that same path will serve the generated
+  //    page, just like the other pages pre-rendered at build time.
+
+  // See the documentation on fallback for more.
 };
 
 // Here we're fetching the necessary data for the blog post with the given
